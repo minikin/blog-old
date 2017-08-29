0001c7b55125498bebdbc997dc7791277221c2b4
minikin <djminikin@gmail.com>
2017-08-29 23:30:05 +0200

    Add SwiftWithReactNative project

---
+++ Callbacks/Callbacks/Views/DummyCell.swift
@@ -0,0 +1,13 @@
+//
+//  DummyCell.swift
+//  Callbacks
+//
+//  Created by Sasha Minikin on 8/29/17.
+//  Copyright © 2017 Minikin.me. All rights reserved.
+//
+
+import UIKit
+
+class DummyCell: UICollectionViewCell {
+    
+}
